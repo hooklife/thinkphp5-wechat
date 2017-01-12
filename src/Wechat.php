91626286faa8c735f791a8cdc7be4af351cdd2ab
hooklife <miaogaopeng123@gmail.com>
2017-01-12 15:58:11 +0800

    添加命令行功能

--- src/Wechat.php
+++ src/Wechat.php
@@ -13,15 +13,33 @@ use think\Config;
 /**
  * Class Wechat  微信类
  * @package Hooklife\ThinkphpWechat
- * @method static \EasyWeChat\Foundation\Application server //服务端
- * @method static \EasyWeChat\Foundation\Application broadcast //群发
- * @method static \EasyWeChat\Foundation\Application notice //模板消息
- * @method static \EasyWeChat\Foundation\Application user //用户
- * @method static \EasyWeChat\Foundation\Application user_tag //用户标签
- * @method static \EasyWeChat\Foundation\Application user_group //用户组
- * @method static \EasyWeChat\Foundation\Application oauth //网页授权
- * @method static \EasyWeChat\Foundation\Application material //素材管理（永久素材）
- * @method static \EasyWeChat\Foundation\Application material_temporary //素材管理（临时素材）
+ * @method static \EasyWeChat\Core\AccessToken                access_token
+ * @method static \EasyWeChat\Server\Guard                    server
+ * @method static \EasyWeChat\User\User                       user
+ * @method static \EasyWeChat\User\Tag                        user_tag
+ * @method static \EasyWeChat\User\Group                      user_group
+ * @method static \EasyWeChat\Js\Js                           js
+ * @method static \Overtrue\Socialite\SocialiteManager        oauth
+ * @method static \EasyWeChat\Menu\Menu                       menu
+ * @method static \EasyWeChat\Notice\Notice                   notice
+ * @method static \EasyWeChat\Material\Material               material
+ * @method static \EasyWeChat\Material\Temporary              material_temporary
+ * @method static \EasyWeChat\Staff\Staff                     staff
+ * @method static \EasyWeChat\Url\Url                         url
+ * @method static \EasyWeChat\QRCode\QRCode                   qrcode
+ * @method static \EasyWeChat\Semantic\Semantic               semantic
+ * @method static \EasyWeChat\Stats\Stats                     stats
+ * @method static \EasyWeChat\Payment\Merchant                merchant
+ * @method static \EasyWeChat\Payment\Payment                 payment
+ * @method static \EasyWeChat\Payment\LuckyMoney\LuckyMoney   lucky_money
+ * @method static \EasyWeChat\Payment\MerchantPay\MerchantPay merchant_pay
+ * @method static \EasyWeChat\Reply\Reply                     reply
+ * @method static \EasyWeChat\Broadcast\Broadcast             broadcast
+ * @method static \EasyWeChat\Card\Card                       card
+ * @method static \EasyWeChat\Device\Device                   device
+ * @method static \EasyWeChat\ShakeAround\ShakeAround         shakearound
+ * @method static \EasyWeChat\User\MiniAppUser                mini_app_user
+ * @method static \EasyWeChat\OpenPlatform\OpenPlatform       open_platform
  *
  */
 class Wechat
---
+++ src/helper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hooklife
+ * Date: 2017/1/12
+ * Time: 15:32
+ */
+\think\Console::addDefaultCommands([
+    \Hooklife\ThinkphpWechat\Command\SendConfig::class
+]);
+
+if (!function_exists('wechat')) {
+    /**
+     * @param $name
+     * @return mixed
+     */
+    function wechat($name)
+    {
+        return call_user_func([\Hooklife\ThinkphpWechat\Wechat::class,$name]);
+    }
+}
