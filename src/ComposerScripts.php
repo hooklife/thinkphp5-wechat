074175353e2a7e50c823a99647d2e43f06471439
hooklife <miaogaopeng123@gmail.com>
2017-01-12 15:08:59 +0800

    完善功能

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -7,14 +7,25 @@
  */
 namespace  Hooklife\ThinkphpWechat;
 
+/**
+ * composer install脚本
+ * Class ComposerScripts
+ * @package Hooklife\ThinkphpWechat
+ */
 class ComposerScripts{
+    /*
+     * 发送默认配置文件到TP目录下
+     */
     public static function sendConfig()
     {
+        //获取默认配置文件
         $config = file_get_contents('config.php');
-        $sendConfigPath = CONF_PATH.'/extra/wechat.php';
-        if(file_exists($sendConfigPath)){
-            return true;
-        }
-        file_put_contents($sendConfigPath,$config);
+
+        $sendConfigPath = CONF_PATH.'extra/';
+        $sendConfigFile = $sendConfigPath.'wechat.php';
+
+        mkdir($sendConfigPath);
+
+        return file_exists($sendConfigFile) ? true : file_put_contents($sendConfigFile,$config);
     }
 }
--- src/Wechat.php
+++ src/Wechat.php
@@ -10,9 +10,24 @@ namespace  Hooklife\ThinkphpWechat;
 use EasyWeChat\Foundation\Application;
 use think\Config;
 
-class Wechat{
-
+/**
+ * Class Wechat  微信类
+ * @package Hooklife\ThinkphpWechat
+ * @method static \EasyWeChat\Foundation\Application server //服务端
+ * @method static \EasyWeChat\Foundation\Application broadcast //群发
+ * @method static \EasyWeChat\Foundation\Application notice //模板消息
+ * @method static \EasyWeChat\Foundation\Application user //用户
+ * @method static \EasyWeChat\Foundation\Application user_tag //用户标签
+ * @method static \EasyWeChat\Foundation\Application user_group //用户组
+ * @method static \EasyWeChat\Foundation\Application oauth //网页授权
+ * @method static \EasyWeChat\Foundation\Application material //素材管理（永久素材）
+ * @method static \EasyWeChat\Foundation\Application material_temporary //素材管理（临时素材）
+ *
+ */
+class Wechat
+{
     protected static $app;
+
     public static function app()
     {
         if (!isset(self::$app)) {
@@ -20,5 +35,10 @@ class Wechat{
             self::$app = new Application($options);
         }
         return self::$app;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        return self::app()->$name;
     }
 }
